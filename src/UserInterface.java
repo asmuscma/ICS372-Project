8641330323ad45ab91b26cd6f9411096eaae18e7
asmuscma <asmuscma@gmail.com>
2020-04-16 13:58:22 -0500

    Modified UserInterface and created Company
    Added a few vital functions to the UserInterface class and created the Company class. There will most likely be need to add more functions as private static final ints

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,4 +1,193 @@
+/** 
+ * 
+ * @author Colin Asmus, Phong Chang, Ronald Marita, Zion Tran
+ * */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+/**
+ * This class implements the user interface for the Company project. The
+ * commands are encoded as integers using a number of static final variables. A
+ * number of utility methods exist to make it easier to parse the input.
+ * */
 public class UserInterface {
+	private static UserInterface userInterface;
+	private BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	private static Company company;
+	private static final int EXIT = 0;
+	private static final int ADD_CUSTOMER = 1;
+	private static final int ADD_APPLIANCE = 2;
+	private static final int ADD_ORDER = 3;
+	private static final int ADD_BACKORDER = 4;
+	private static final int ADD_REPAIRPLAN = 5;
+	private static final int ADD_ITEM = 6;
+	private static final int GET_ORDERS = 7;
+	private static final int PROCESS_BACKORDERS = 8;
+	private static final int PROCESS_REPAIRPLANS = 9;
+	private static final int GET_REVENUE = 10;
+	private static final int SAVE = 11;
+	private static final int RETRIEVE = 12;
+	private static final int PRINT_FORMATTED = 13;
+	private static final int HELP = 14;
+	
+	/** 
+	 * Private constructor to implement the singleton pattern. It looks for any saved data, otherwise it will get a singleton Company object.
+	 */
+	private UserInterface() {
+		if (yesOrNo("Look for saved data and use it?")) {
+			retrieve();
+		} else {
+			company = Company.instance();
+		}
+	}
+	
+	/**
+	 * Supports the singleton pattern
+	 * 
+	 * @return the singleton object
+	 */
+	public static UserInterface instance() {
+		if (userInterface == null) {
+			return userInterface = new UserInterface();
+		} else {
+			return userInterface;
+		}
+	}
 
+	/**
+	 * Gets a token after prompting
+	 * 
+	 * @param prompt
+	 *            - whatever the user wants as prompt
+	 * @return - the token from the keyboard
+	 * 
+	 */
+	public String getToken(String prompt) {
+		do {
+			try {
+				System.out.println(prompt);
+				String line = reader.readLine();
+				StringTokenizer tokenizer = new StringTokenizer(line, "\n\r\f");
+				if (tokenizer.hasMoreTokens()) {
+					return tokenizer.nextToken();
+				}
+			} catch (IOException ioe) {
+				System.exit(0);
+			}
+		} while (true);
+	}
+
+	/**
+	 * Queries for a yes or no and returns true for yes and false for no
+	 * 
+	 * @param prompt
+	 *            The string to be prepended to the yes/no prompt
+	 * @return true for yes and false for no
+	 * 
+	 */
+	private boolean yesOrNo(String prompt) {
+		String more = getToken(prompt + " (Y|y)[es] or anything else for no");
+		if (more.charAt(0) != 'y' && more.charAt(0) != 'Y') {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Converts the string to a number
+	 * 
+	 * @param prompt
+	 *            the string for prompting
+	 * @return the integer corresponding to the string
+	 * 
+	 */
+	public int getNumber(String prompt) {
+		do {
+			try {
+				String item = getToken(prompt);
+				Integer number = Integer.valueOf(item);
+				return number.intValue();
+			} catch (NumberFormatException nfe) {
+				System.out.println("Please input a number ");
+			}
+		} while (true);
+	}
+
+	/**
+	 * Prompts for a date and gets a date object
+	 * 
+	 * @param prompt
+	 *            the prompt
+	 * @return the data as a Calendar object
+	 */
+	public Calendar getDate(String prompt) {
+		do {
+			try {
+				Calendar date = new GregorianCalendar();
+				String item = getToken(prompt);
+				DateFormat dateFormat = SimpleDateFormat
+						.getDateInstance(DateFormat.SHORT);
+				date.setTime(dateFormat.parse(item));
+				return date;
+			} catch (Exception fe) {
+				System.out.println("Please input a date as mm/dd/yy");
+			}
+		} while (true);
+	}
+
+	/**
+	 * Prompts for a command from the keyboard
+	 * 
+	 * @return a valid command
+	 * 
+	 */
+	public int getCommand() {
+		do {
+			try {
+				int value = Integer.parseInt(getToken("Enter command:" + HELP
+						+ " for help"));
+				if (value >= EXIT && value <= HELP) {
+					return value;
+				}
+			} catch (NumberFormatException nfe) {
+				System.out.println("Enter a number");
+			}
+		} while (true);
+	}
+
+	/**
+	 * Displays the help screen
+	 * 
+	 */
+	public void help() {
+		System.out
+				.println("Enter a number between 0 and 12 as explained below:");
+		System.out.println(EXIT + " to Exit\n");
+		System.out.println(ADD_CUSTOMER + " to add a customer");
+		System.out.println(ADD_APPLIANCE + " to add appliances");
+		System.out.println(ADD_ORDER + " to add a transaction/order");
+		System.out.println(ADD_BACKORDER + " to place a backorder");
+		System.out.println(ADD_REPAIRPLAN + " to add a repair plan");
+		System.out.println(ADD_ITEM + " to add items to the inventory");
+		System.out.println(GET_ORDERS + " to print the list of transactions");
+		System.out.println(PROCESS_BACKORDERS + " to process any backorders");
+		System.out.println(PROCESS_REPAIRPLANS + " to any repair plans");
+		System.out.println(GET_REVENUE + " to print total revenue");
+		System.out.println(SAVE + " to  save data");
+		System.out.println(RETRIEVE + " to  retrieve");
+		System.out.println(PRINT_FORMATTED + " to  print items formatted");
+		System.out.println(HELP + " for help");
+	}
+	
+	public static void main(int[] args) {
+		
+	}
 }
