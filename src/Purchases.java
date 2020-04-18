5155843d1ce43c073c954c415d5221e47faf4264
PopcornTheBearCat <54016398+PopcornTheBearCat@users.noreply.github.com>
2020-04-18 11:38:37 -0500

    Update Purchases.java

--- src/Purchases.java
+++ src/Purchases.java
@@ -1,64 +1,6 @@
 
 import java.io.Serializable;
 import java.util.Calendar;
-<<<<<<< HEAD
-import java.util.GregorianCalendar;
-
-public abstract class Purchases implements Serializable {
-	private static final long serialVersionUID = 1L;
-	private String orderID;
-	private String customerID;
-	private String applianceID;
-	private double orderCost;
-	private int quantity;
-	private Calendar date;
-
-	public Purchases(String orderID, String customerID, String applianceID, double orderCost, int quantity, Calendar date) {
-		this.orderID = orderID;
-		this.customerID = customerID;
-		this.applianceID = applianceID;
-		this.orderCost = orderCost;
-		this.quantity = quantity;
-		date = new GregorianCalendar();
-		date.setTimeInMillis(System.currentTimeMillis());
-	}
-
-	public boolean onDate(Calendar date) {
-		return ((date.get(Calendar.YEAR) == this.date.get(Calendar.YEAR))
-				&& (date.get(Calendar.MONTH) == this.date.get(Calendar.MONTH))
-				&& (date.get(Calendar.DATE) == this.date.get(Calendar.DATE)));
-	}
-
-	public String getID() {
-		return orderID;
-	}
-
-	public String getCustomerID() {
-		return customerID;
-	}
-
-	public String getApplianceID() {
-		return applianceID;
-	}
-
-	public double getOrderCost() {
-		return orderCost;
-	}
-
-	public String getDate() {
-		return date.get(Calendar.MONTH) + "/" + date.get(Calendar.DATE) + "/" + date.get(Calendar.YEAR);
-	}
-
-	public int getQuantity() {
-		return quantity;
-	}
-
-	@Override
-	public String toString() {
-		return "Order [customerID=" + customerID + ", applianceID=" + applianceID + ", orderCost=" + orderCost + "]";
-	}
-
-
 
 public class Purchases extends Order implements Matchable<String>, Serializable {
 	private static final long serialVersionUID = 1L;
@@ -74,4 +16,4 @@ public class Purchases extends Order implements Matchable<String>, Serializable 
 		return this.getID().equals(key);
 	}
 
-}}
+}
