f1753ddcd749d00e02899776aecb774ab947e87d
ronald <ronald.marita.9594@my.nhcc.edu>
2020-04-18 09:33:04 -0500

    Create BackOrder.java

---
+++ src/BackOrder.java
@@ -0,0 +1,21 @@
+import java.io.Serializable;
+import java.util.Calendar;
+
+public class BackOrder extends Order implements Matchable<String>, Serializable {
+	private static final long serialVersionUID = 1L;
+
+	public BackOrder(String orderID, String customerID, String applianceID, double orderCost, int quantity,
+			Calendar date) {
+		super(orderID, customerID, applianceID, orderCost, quantity, date);
+	}
+
+	public boolean onBackOrder() {
+		return true;
+	}
+
+	@Override
+	public boolean matches(String key) {
+		return this.getID().equals(key);
+	}
+
+}
