afbd8ef2dbdeede348593d3cfa1df37ee6ec890c
ChangPhong <changphong@yahoo.com>
2020-04-17 23:41:11 -0500

    Update BackOrder.java

--- src/BackOrder.java
+++ src/BackOrder.java
@@ -1,4 +1,16 @@
+import java.io.Serializable;
+import java.util.Calendar;
 
-public class BackOrder {
+public class BackOrder extends Order implements Matchable<String>, Serializable{
+	private static final long serialVersionUID = 1L;
+
+	public BackOrder(String orderID, String customerID, String applianceID, double orderCost, int quantity,
+			Calendar date) {
+		super(orderID, customerID, applianceID, orderCost, quantity, date);
+	}
+	
+	public boolean onBackOrder() {
+		if()
+	}
 
 }
