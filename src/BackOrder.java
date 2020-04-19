3be7660f3b7a25dd6d50345125c4430f8039aec2
PopcornTheBearCat <54016398+PopcornTheBearCat@users.noreply.github.com>
2020-04-18 19:43:35 -0500

    BackOrder and Customer

--- src/BackOrder.java
+++ src/BackOrder.java
@@ -3,14 +3,19 @@ import java.util.Calendar;
 
 public class BackOrder extends Order implements Matchable<String>, Serializable {
 	private static final long serialVersionUID = 1L;
+	boolean backOrderStatus = false;
 
 	public BackOrder(String orderID, String customerID, String applianceID, double orderCost, int quantity,
 			Calendar date) {
 		super(orderID, customerID, applianceID, orderCost, quantity, date);
 	}
 
-	public boolean onBackOrder() {
-		return true;
+	public void setBackOrderStatus(boolean status) {
+		backOrderStatus = status;
+	}
+
+	public boolean getBackOrderStatus() {
+		return backOrderStatus;
 	}
 
 	@Override
